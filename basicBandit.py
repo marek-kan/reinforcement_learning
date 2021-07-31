2e9bf028e9bae2da0bd42d596156c94a792e2236
Marek Kancir <kancir.marek@gmail.com>
2021-07-31 12:50:01 +0200

    add looping plus comment on findings

--- basicBandit.py
+++ basicBandit.py
@@ -79,7 +79,7 @@ class MultiArmedBandit():
         o = 0
         for i in range(n_steps):
             if not stationary:
-                self.update_reward_dists(m=np.random.uniform(-3, 3))
+                self.update_reward_dists()
             if decay_step_size:
                 step_size = step_size * (1 / (1 + 0.001 * i))
                 
@@ -91,46 +91,67 @@ class MultiArmedBandit():
                 a = np.random.choice(np.flatnonzero(self.Q == self.Q.max()))
             r = self.dists[a].rvs()
             self.N[a] = self.N[a] + 1
-            # self.Q[a] = self.Q[a] + 1/self.N[a] * (r - self.Q[a])
             self.Q[a] = self.Q[a] + step_size * (r - self.Q[a])
             self.final_rewards.append(r)
     
     def update_reward_dists(self, m=0, std=1):
         for i, d in enumerate(self.dists):
             params = d.kwds
-            random_change = stat.norm.rvs(loc=m, scale=std)
+            random_change_m = np.random.uniform(-3, 3)
+            random_change_s = np.random.uniform(-1, 1)
+            new_std = params['scale']+random_change_s
+            if new_std<0:
+                new_std = 0
             self.dists[i] = stat.norm(
-                loc=params['loc']+random_change,
+                loc=params['loc']+random_change_m,
                 scale=params['scale']
             )
 
 if __name__ == '__main__':
-    np.random.seed(123)
-    n_steps = 1000
-    n_dist = 5
+    # np.random.seed(123)
+    n_steps = 5000
+    n_dist = 10
     e = 0.1
     
-    bandit = MultiArmedBandit(n_steps, e, n_dist)
-    bandit.call_sample_avg(False)
-    
-    print('Distributions args:', [d.kwds for d in bandit.dists])
-    print('\nAction values:', bandit.Q)
-    print('\nNumber of action choices:', bandit.N)
-    
-    plt.plot(range(n_steps), np.cumsum(bandit.final_rewards))
-    plt.title(f'Cum sum of rewards - sample avg, {np.sum(bandit.final_rewards)}')
-    plt.xlabel('step')
-    plt.ylabel('Sum')
-    plt.show()
-    plt.close()
-    
-    bandit = MultiArmedBandit(n_steps, e, n_dist)
-    bandit.call_weighted_avg(False, 0.5, decay_step_size=True)
-    print('\nAction values:', bandit.Q)
-    print('\nNumber of action choices:', bandit.N)
-    plt.plot(range(n_steps), np.cumsum(bandit.final_rewards))
-    plt.title(f'Cum sum of rewards - weighted avg, {np.sum(bandit.final_rewards)}')
-    plt.xlabel('step')
-    plt.ylabel('Sum')
-    plt.show()
-    plt.close()
+    res_simple = []
+    res_weighted = []    
+    for i in range(1):
+        bandit = MultiArmedBandit(n_steps, e, n_dist)
+        bandit.call_sample_avg(False)
+        
+        print('Distributions args:', [d.kwds for d in bandit.dists])
+        print('\nAction values:', bandit.Q)
+        print('\nNumber of action choices:', bandit.N)
+        final_reward = np.sum(bandit.final_rewards)
+        res_simple.append(final_reward)
+        
+        plt.plot(range(n_steps), np.cumsum(bandit.final_rewards))
+        plt.title(f'Cum sum of rewards - sample avg, {final_reward}')
+        plt.xlabel('step')
+        plt.ylabel('Sum')
+        plt.show()
+        plt.close()
+        
+        bandit = MultiArmedBandit(n_steps, e, n_dist)
+        bandit.call_weighted_avg(False, 0.1, decay_step_size=False)
+        final_reward = np.sum(bandit.final_rewards)
+        res_weighted.append(final_reward)
+        
+        print('\nAction values:', bandit.Q)
+        print('\nNumber of action choices:', bandit.N)
+        plt.plot(range(n_steps), np.cumsum(bandit.final_rewards))
+        plt.title(f'Cum sum of rewards - weighted avg, {final_reward}')
+        plt.xlabel('step')
+        plt.ylabel('Sum')
+        plt.show()
+        plt.close()
+        
+"""
+Takze moje zistenie je ze vo vseobecnosti (tychto podmienok) je sample mean 
+stabilnejsi a trade off var a proftu nestoji za konstantny step size. Urcite nie
+s postupnym znizovanim. Pri konstantnom cez vsetky casove kroky vie byt vykonnejsi. 
+Takze na to aby tato estimacia bola lepsia treba dost specificke podmienky plus spravne 
+nastavit epsilon a step size.
+
+Lepsi vykon sa prejavi hlave na long rune pri nestacionarnych problemoch.
+"""

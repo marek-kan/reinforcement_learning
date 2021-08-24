4cc31961d8bee5c8d5e087ec162c6e5850bde4ed
Marek Kancir <kancir.marek@gmail.com>
2021-08-24 21:00:16 +0200

    fix car rental problem

--- dyn_prog/car_rental.py
+++ dyn_prog/car_rental.py
@@ -111,8 +111,8 @@ def P_calculate(joint_prob):
                         for customer_B in range(21):  # total customers come at the end of the day at B
                             for returned_car_A in range(21):  # total cars returned at the end of the day at A
                                 for returned_car_B in range(21):  # total cars returned at the end of the day at B
-                                    rented_cars_a = min(customer_A, state_value_A-action) # lets say 20 customers and 1-(-5)
-                                    rented_cars_b = min(customer_B, state_value_B+action)
+                                    rented_cars_a = min(customer_A, state_value_A - action) # lets say 20 customers and 1-(-5)
+                                    rented_cars_b = min(customer_B, state_value_B + action)
                                     
                                     value_A_Changed = min(20, state_value_A - rented_cars_a + returned_car_A - action)
                                     value_B_Changed = min(20, state_value_B - rented_cars_b + returned_car_B + action)
@@ -130,8 +130,7 @@ def P_calculate(joint_prob):
                     pickle.dump(P, f, protocol=-1)
     return 
 
-def policy_eval(V, policy, P, 
-                theta=0.01, gamma=0.9):
+def policy_eval(V, policy, theta=0.01, gamma=0.9):
     """
     Computes value functions for given policy. "Prediction problem"
 
@@ -141,8 +140,6 @@ def policy_eval(V, policy, P,
         Value function.
     policy : Dict
         Policy.
-    P : Dict
-        State - action - reward dict.
     theta : float, optional
         Tolerance of approx. The default is 0.01.
     gamma : float, optional
@@ -174,7 +171,7 @@ def policy_eval(V, policy, P,
             return V
         counter += 1
         
-def policy_improvement(V, P, policy={}, gamma=0.9):
+def policy_improvement(V, policy={}, gamma=0.9):
     """
     Improves policy based on original value function
 
@@ -184,8 +181,6 @@ def policy_improvement(V, P, policy={}, gamma=0.9):
         Value function.
     policy : Dict
         Policy.
-    P : Dict
-        State - action - reward dict.
     gamma : float, optional
         Discounting rate in value function estimate. The default is 0.9.
 
@@ -196,26 +191,27 @@ def policy_improvement(V, P, policy={}, gamma=0.9):
 
     """
     counter = 0
-    while True:
+    while True and counter <= 10e3:
         policy_stable=True
         for k, old_action in policy.items():
             q = [0] * 11
             state_a, state_b = k
             for a in range(-5, 6):
                 P = pickle.load(open(f'P_{state_a}_{state_b}.pkl', 'rb'))
-                p = P[(state_a, state_b, a)]
                 idx = a + 5
                 if (a <= state_a and -a <= state_b and a + state_b <= 20 and -a + state_a <= 20):
-                    for states, reward, prob in p.items():
-                        q[idx] = prob * (reward + gamma*V[states])
+                    p = P[(state_a, state_b, a)]
+                    for (states, reward), prob in p.items():
+                        q[idx] += prob * (reward + gamma*V[states])
                 else:
                     q[idx] = -999 # not possible action
             policy[k] = np.argmax(q) - 5
             if policy[k] != old_action:
                 policy_stable = False
+            counter += 1
         if policy_stable:
             return policy
-    return
+    return policy
 
 def train():
     # joint_prob = init_prob()
@@ -232,14 +228,17 @@ def train():
         print('Training step:', i+1)
         V = policy_eval(V, policy)
         policy = policy_improvement(V, policy)
-        
+        for i in range(21):
+            print("i = " + str(i))
+            for j in range(21):
+                print("  " + str(policy[i, j]))
     return V, policy
     
     
     
-# V, policy = train()
-joint_prob = init_prob()
-P_calculate(joint_prob)
+V, policy = train()
+# joint_prob = init_prob()
+# P_calculate(joint_prob)
     
     
     
